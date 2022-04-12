653ed65d3337cf230e209233f6576fceeb89ba5d
NindyBun <andyco5665@gmail.com>
2022-04-12 05:31:22 -0700

    j

---
+++ prototypes/entities.lua
@@ -0,0 +1,30 @@
+data.raw["inserter"]["inserter"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
+data.raw["inserter"]["fast-inserter"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
+data.raw["inserter"]["filter-inserter"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
+data.raw["inserter"]["stack-inserter"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
+data.raw["inserter"]["stack-filter-inserter"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
+data.raw["lab"]["lab"].energy_source = {
+    type = "burner",
+    fuel_inventory_size = 1,
+    render_no_power_icon = false
+}
